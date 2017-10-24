1e9a0792edbd56d2e280efee295f175e4dcacd9e
tsitsi <tsitsimunikwa97@gmail.com>
2017-10-24 19:44:00 +0200

    commmit #2

--- run.py
+++ run.py
@@ -1,1 +1,7 @@
+# this file runs the application and allows you to view the application from the web browser 
 
+from ParentalControl import app
+import os
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
