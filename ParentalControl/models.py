30e8727266c56f8482eead5898ed1838566172ba
tsitsi <tsitsimunikwa97@gmail.com>
2017-10-24 19:51:56 +0200

    package

--- ParentalControl/models.py
+++ ParentalControl/models.py
@@ -1,1 +1,9 @@
+from datetime import date
+
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from ParentalControl import db 
+
+
 
--- ParentalControl/views.py
+++ ParentalControl/views.py
@@ -1,1 +1,7 @@
+from flask import render_template
 
+from ParentalControl import app
+
+@app.route('/')
+def index():
+    return render_template('index.html')
